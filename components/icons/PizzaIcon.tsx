c8b0a4e0179c5acc3fc870198ed2ac80690fb329
Manu <manudesignsforyou@gmail.com>
2025-10-31 12:03:18 +0100

    fix: Update PizzaIcon SVG path data
    The SVG path data for the PizzaIcon component has been updated to render a more accurate pizza slice shape and associated details. This change improves the visual representation of the icon.

--- components/icons/PizzaIcon.tsx
+++ components/icons/PizzaIcon.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const PizzaIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="M15 11h.01"/>
-        <path d="M11 15h.01"/>
-        <path d="M16 16h.01"/>
-        <path d="M21.43 11.43 12.57 2.57a1 1 0 0 0-1.42 0L2.57 11.43a1 1 0 0 0 0 1.42l8.86 8.86a1 1 0 0 0 1.42 0l8.86-8.86a1 1 0 0 0 0-1.42Z"/>
-        <path d="M9 12a5 5 0 0 1 5-5"/>
+        <path d="M12 2 L2 12 L12 22 C 17.5 22 22 17.5 22 12 C 22 6.5 17.5 2 12 2 Z"/>
+        <path d="M12 2 V 12 L 22 12"/>
+        <path d="M7 7 C 7.5 7.5 8.5 7.5 9 7"/>
+        <path d="M6 12 C 6.5 12.5 7.5 12.5 8 12"/>
+        <path d="M16 8 C 16.5 8.5 17.5 8.5 18 8"/>
     </svg>
 );
 
